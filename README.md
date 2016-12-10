d94fde280e8c309ba5310613a8924f996cf1a32b
mawfia <michael.a.williams@mawfia.com>
2016-12-10 11:25:12 -0700

    Initial Commit, fully working including stretch goals

---
+++ README.md
@@ -0,0 +1,38 @@
+# Calculator
+Individual Project created 10 December 2016
+
+INTRODUCTION:
+--------------------
+This is a JavaScript/jQuery project designed, built, and deployed to cloud services in 1 day.
+
+HOW TO USE:
+---------------------
+
+
+
+TECHNOLOGY USED:
+-----------------
+1.  All back-end logic and data storage was executed using Java, JPA, and MySQL.
+
+2.  Search and sort features were implemented using Java8 Lambda Expressions and JPA.
+
+3.  Maven was used to manage the libraries for XML dependencies.
+
+4.  Bootstrap was used for front-end design and operations such as regular expressions for exception prevention and dynamically populated fields and drop-down lists were implemented using HTML5 and CSS.
+
+5.  Spring Validation was used for form filtering and account creation.
+
+6.  Apache Tomcat is used as the server for execution and JSP, JSPTL, and JavaScript handle server/client requests.
+
+7.  Spring/MVC is the framework used for dynamic management of objects with Spring Entity Manager.
+
+8.  The application is deployed on a AWS account for cloud services.
+
+This web application may be viewed at: http://mawfia.com:8080/calculator/
+
+Current Maintainer:
+ * Michael Williams
+
+This project has been sponsored by:
+Skill Distillery, a Division of Batky-Howell, LLC
+http://skilldistillery.com/
---
+++ calculator.js
@@ -0,0 +1,77 @@
+var operand1 = [];
+var operand2 = [];
+var operation = null;
+
+$('button').click(function(e){
+	
+	mapValue($(this).val());
+	
+});
+
+$('body').keypress(function(e){
+
+      getValue(e);
+
+});
+
+function mapValue(value){
+	
+	switch(value){
+		case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+			if(!operation) { operand1.push(value); display(operand1.join(''), "", "", null); console.log(operand1.join('')); }
+			else { operand2.push(value); display(operand1.join(''), operand2.join(''), operation); console.log(operand2.join('')); }			
+			break;
+		case '+': case '-': case '/': case '*':
+			operation = value; 
+			display(operand1.join(''), "", operation);
+			break; 
+		case '.': 
+			if(!operation && operand1.indexOf(value) == -1) {operand1.push(value); display(operand1.join(''), "", null, null); break; }
+			else if(operand2 && operand2.indexOf(value) == -1) {operand2.push(value); display(operand1.join(''), operand2.join(''), operation, null); break; }
+		case 'C': clear(); display(operand1, operand2, null, null); break; 
+		case 'B':  
+				if(operand1.length > 0 && operation && operand2.length > 0) { operand2.pop(); display(operand1.join(''), operand2.join(''), operation); } 
+				else if( operand1.length > 0 && operation) { operation = null; display(operand1.join(''), operand2.join(''), operation); }
+				else if( operand1.length > 0 ) { operand1.pop(); display(operand1.join(''), "", ""); }
+			break;
+		case '=': 
+			calculate(parseFloat(operand1.join(''), 10), parseFloat(operand2.join(''), 10), operation);
+			break;
+		default: break;
+	}
+}
+
+function getValue(e){
+
+	console.log(e.key + " " + e.which + " " + e.code);
+	//if(e.which > 47 && e.which < 58) mapValue(e.key);
+	if(e.which === 13) mapValue('=');
+	else if(e.which === 0) mapValue('C');
+	else mapValue(e.key);
+}
+
+function display(operand1, operand2, operation, result){
+	if (result) $('#result').text(operand1 + " " + operation + " " + operand2 + " = " + result);
+	else if(operand1.length > 0 && operand2.length > 0 && operation)	$('#result').text(operand1 + " " + operation + " " + operand2);
+	else if(operand1.length > 0 && operation) $('#result').text(operand1 + " " + operation);
+	else if(operand1.length > 0) $('#result').text(operand1);
+	else $('#result').text('');
+}
+
+function calculate(num1, num2, operation){
+	switch(operation){
+		case '+': display(num1, num2, operation, num1 + num2); break;
+		case '-': display(num1, num2, operation, num1 - num2); break;
+		case '*': display(num1, num2, operation, num1 * num2); break;
+		case '/': display(num1, num2, operation, num1 / num2); break;
+		default: break;
+	}
+	clear();
+}
+
+function clear(){
+		operation = null;
+		operator = null;
+		for(var i = operand1.length; i >= 0; i--) operand1.pop();
+		for(var i = operand2.length; i >= 0; i--) operand2.pop();
+}
